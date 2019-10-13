e2834fb135127cf509f79b02857ce7def3d4a69e
Maksim Korzh <freesoft.for.people@gmail.com>
2019-10-13 15:36:43 +0300

    wrapped crawler into a class, added store into mongodb feature

--- crawler.py
+++ crawler.py
@@ -1,72 +1,109 @@
 #
-# Simple web crawler
+# Web crawler module
 #
-
-# TODO:
-# 1. Break more links are available
-# 2. Try to avoid duplicatate links responses
-# 3. Try to draft a simple site(pages) to debug the crawler
 
 # import libraries
 import json
 import requests
 from bs4 import BeautifulSoup
+import pymongo
 
-#start_url = 'http://quotes.toscrape.com'
-start_url = 'https://code-monkey-king.herokuapp.com'
 
-data = []
-
-def crawl(url, depth):
-    try:
-        print('Crawling url: "%s" at depth: %d' % (url, depth))
-        response = requests.get(url)
-    except:
-        print('Failed to perform HTTP GET request on "%s"\n' % url)
-        return
+class Crawler():
+    # connect to cloud mongo
+    connect_uri = 'mongodb+srv://cmk:342124@todolist-c483l.gcp.mongodb.net/search?retryWrites=true&w=majority'
+    client = pymongo.MongoClient(connect_uri)
     
-    content = BeautifulSoup(response.text, 'lxml')
+    # create db client
+    db = client.search
 
-    title = content.find('title').text
-    description = content.get_text()
-    
-    if description is None:
-        description = ''
-    else:
-        description = description.strip().replace('\n', ' ')
-    
-    result = {
-        'url': url,
-        'title': title,
-        'description': description
-    }
-    
-    data.append(result)
-    
-    # return when depth is exhausted
-    if depth == 0:
-        return
+    # search results storage
+    search_results = []
 
-    try:
-        links = content.findAll('a')
-    except:
-        return
-
-    for link in links:
+    # crawl domain
+    def crawl(self, url, depth):
+        # try to perform HTTP GET request
         try:
-            if 'http' not in link['href']:
-                follow_url = url + link['href']
-            else:
-                follow_url = link['href']
+            print('Crawling url: "%s" at depth: %d' % (url, depth))
+            response = requests.get(url, headers={'user-agent': 'code-monkey-search'})
+        
+        # return otherwise
+        except:
+            print('Failed to perform HTTP GET request on "%s"\n' % url)
+            return
+        
+        # parse page content
+        content = BeautifulSoup(response.text, 'lxml')
 
-            crawl(follow_url, depth - 1)
-        except KeyError:
-            pass
-    
-    return
+        # try to extract page title and description
+        try:
+            title = content.find('title').text
+            description = ''
+            
+            for tag in content.findAll():
+                if tag.name == 'p':
+                    description += tag.text.strip().replace('\n', '')
+        # return otherwise
+        except:
+            return
+        
+        # store the result structure
+        result = {
+            'url': url,
+            'title': title,
+            'description': description
+        }
+        
+        # store the result
+        self.search_results.append(result)
+        
+        # return when depth is exhausted
+        if depth == 0:
+            return
 
-crawl(start_url, 2)
-print(len(data))
+        # extract all the available links on the page
+        links = content.findAll('a')
+
+        # loop over links
+        for link in links:
+            # try to crawl links recursively
+            try:
+                # use only links starting with 'http'
+                if 'http' in link['href']:
+                    self.crawl(link['href'], depth - 1)
+            
+            # ignore internal links
+            except KeyError:
+                pass
+
+    # write search results to mongodb
+    def insert_results(self):
+        # insert data to mongodb
+        search_results = self.db.search_results
+        search_results.insert_many(self.search_results)
+        search_results.create_index([
+            ('url', pymongo.TEXT),
+            ('title', pymongo.TEXT),
+            ('description', pymongo.TEXT)
+        ], name='search_results', default_language='english')
+
+    def print_data(self):
+        # loop over data list
+        for entry in self.search_results:
+            print(json.dumps(entry))
+
+        print('\nNumber of entries scraped: %d' % len(self.search_results))
+        
+        print(self.db.command('db.search_results.find( { $text: { $search: "Welcome" } } )'))
+
+
+crawler = Crawler()
+
+#crawler.crawl('http://quotes.toscrape.com', 1)
+#crawler.insert_results()
+crawler.print_data()
+
+
 
 
 
